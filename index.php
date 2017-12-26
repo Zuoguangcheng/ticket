7dbcb2bf914f16c04ded0973dd5c5c19f8432d51
zuoguangcheng <zuoguangcheng@shuidao.com>
2017-12-26 16:19:50 +0800

    修改返回数据

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
-// 指定允许其他域名访问    
 header('Access-Control-Allow-Origin:*');    
-// 响应类型    
 header('Access-Control-Allow-Methods:POST');    
-// 响应头设置    
 header('Access-Control-Allow-Headers:x-requested-with,content-type');    
-
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=1.9035");
@@ -20,19 +16,19 @@ foreach ($match[0] as $city) {
   $cityMap = explode('|', $city);
   $citysMap[$cityMap[0]] = $cityMap[1];
 }
-var_dump($_GET);
+
 $from = $_GET['from'];
 $to = $_GET['to'];
 $date = $_GET['date'];
 
-echo json_encode($from);
+// echo json_encode($from);
 
 if (array_key_exists($from, $citysMap) && array_key_exists($to, $citysMap)) {
   getData();
 } else {
   echo '输入的地址不正确';
+  return;
 }
-
 
 function getData()
 {
@@ -98,5 +94,5 @@ function handleData($data)
     array_push($dataArray, $data);
   }
 
-  print_r($dataArray);
+  echo json_encode($dataArray);
 }
